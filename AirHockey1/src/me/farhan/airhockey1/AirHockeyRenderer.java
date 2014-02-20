e68d2b0b28c7d5228cf083971c7c7ea44d440733
farhan-faruque <russel.cse05@gmail.com>
2014-02-20 12:57:13 +0600

    change

--- AirHockey1/src/me/farhan/airhockey1/AirHockeyRenderer.java
+++ AirHockey1/src/me/farhan/airhockey1/AirHockeyRenderer.java
@@ -1,5 +1,10 @@
 package me.farhan.airhockey1;
 
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -8,20 +13,22 @@ import android.opengl.GLSurfaceView.Renderer;
 public class AirHockeyRenderer  implements Renderer{
 
 	@Override
-	public void onDrawFrame(GL10 gl) {
-		
+	public void onDrawFrame(GL10 gl) 
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) 
+	{
+		glViewport(0, 0, width, height);
 		
 	}
 
 	@Override
-	public void onSurfaceChanged(GL10 gl, int width, int height) {
-	
-		
-	}
-
-	@Override
-	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) 
+	{
+		glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
 		
 	}
 
